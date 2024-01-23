f4e8e4d071c740766d22ac5c39d66dd2b0dab2db
Mehedy <mehedypro21@gmail.com>
2024-01-23 23:29:43 +0600

    js file changed

---
+++ index.js
@@ -0,0 +1,14 @@
+// const array = [1, 2, 3, 4, 5];
+// console.log(array);
+
+// const even = array.find(x => x == 5);
+// console.log(even);
+
+// function fun() {
+//     return enjoy => {
+//         console.log('i am in enjoy');
+//     }
+// }
+// const variable = fun();
+// console.log(variable);
+// variable();// it is called clouser
